a8ffaf0b86897c18416a550eec80ac6e990d2fdf
Swayam Gupta <swayamgupta5705367@gmail.com>
2025-01-12 08:32:45 +0530

    3rd commit

--- rango.go
+++ rango.go
@@ -10,8 +10,14 @@ func main() {
 
 	fmt.Println("Rango is working...")
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 5)
 
 	fmt.Println("Closing Rango!")
+
+	var isFinish string = ""
+	for isFinish != "yes" {
+		fmt.Println("Type yes to close")
+		fmt.Scanln(&isFinish)
+	}
 
 }
